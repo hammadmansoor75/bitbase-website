21c37b44fe41f80c150d1ec3c46906416cb8d761
jehanzebrana <jehanzebrana01@gmail.com>
2024-01-23 03:13:30 +0500

    updated

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -3,6 +3,8 @@ import './home.css';
 import Social from './Social';
 import Data from './Data';
 import ScrollDown from './ScrollDown';
+import form from '../../assets/form.png'; // Assuming you have the correct path to your image
+
 export default function Home() {
   return (
     <section className="home section" id="home">
@@ -11,10 +13,35 @@ export default function Home() {
           <div>
             <h1 className='home__heading'>Drive growth at scale for your brand.</h1>
           </div>
-          {/* <Social></Social>
-          <div className="home__img"></div>
-          <Data></Data> */}
+          
+          {/* Form Section */}
+          <div className="form-container">
+            <img src={form} alt="Form Image" className="form-image" />
+            <h2 className="form-heading">What are you interested in?</h2>
+            <p className='small_text'>Select all that apply:</p>
+
+            <div className="form-grid">
+              {/* First Column */}
+              <div className="form-column">
+                <div className="form-service">Service 1</div>
+                <div className="form-service">Service 2</div>
+                <div className="form-service">Service 3</div>
+              </div>
+
+              {/* Second Column */}
+              <div className="form-column">
+                <div className="form-service">Service 4</div>
+                <div className="form-service">Service 5</div>
+                <div className="form-service">Service 6</div>
+              </div>
+            </div>
+
+            <div className="form-button">
+              <button type="button">Next <span>&rarr;</span></button>
+            </div>
+          </div>
         </div>
+        
         <ScrollDown></ScrollDown>
       </div>
     </section>
